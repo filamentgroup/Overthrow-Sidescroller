833492cba15af2becb27f0bddad1e7369fd06b06
Mat Marquis <mat@matmarquis.com>
2013-12-10 15:57:08 -0500

    Stand-alone Sidescroller repo.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - <%= pkg.description %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed <%= pkg.license %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      main: {
+        src: ['src/overthrow-detect.js','src/overthrow-toss.js','src/overthrow-polyfill.js','src/overthrow-init.js','extensions/overthrow-sidescroller.js'],
+        dest: 'dist/<%= pkg.name %>.sidescroller.js'
+      }
+    },
+    qunit: {
+      files: ['test/*.html']
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      main: {
+        src: ['src/overthrow-detect.js','src/overthrow-toss.js','src/overthrow-polyfill.js','src/overthrow-init.js','extensions/overthrow-sidescroller.js'],
+        dest: 'dist/<%= pkg.name %>.sidescroller.min.js'
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
---
+++ dist/overthrow.sidescroller.js
@@ -0,0 +1,2 @@
+/*! overthrow - An overflow:auto polyfill for responsive design. - v0.6.6 - 2013-12-10
+* Copyright (c) 2013 Scott Jehl, Filament Group, Inc.; Licensed MIT */
